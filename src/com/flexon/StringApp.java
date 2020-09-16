0c51d36d4fc2c61961ef93e699b94d6d808bc967
Zoe Woo <zoewoo@MacBook-Pro-8.local>
2020-09-16 17:42:16 -0500

    Add Vowels

--- src/com/flexon/StringApp.java
+++ src/com/flexon/StringApp.java
@@ -4,7 +4,18 @@ public class StringApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//////////Test Vowels////////////
+		////test Input////
+		Vowels testVowels = new Vowels("Can you hear me?");
+		testVowels.countVowels();
+		////User Input////
+		Vowels testVowels1 = new Vowels();
+		testVowels1.countVowels();
+		
+		
+		
+		
 	}
 
 }
